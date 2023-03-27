7e518013aba208bb6ca2ecd73897c2f588d4a6f6
rodriV <utndavid@gmail.com>
2023-03-27 20:33:30 -0300

    ejercicios resueltos

---
+++ app.py
@@ -0,0 +1,11 @@
+from suma import sumar
+
+print("Hola este es un código escrito en mi editor de texto")
+
+nombre = input("Ingrese su nombre")
+
+print(f'El nombre ingresado es {nombre}')
+
+total = sumar(5,5)
+
+print(f'El resultado de la suma es {total}')
